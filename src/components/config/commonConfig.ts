864a04920d9388b80d539cd9e7a1d90f7c36521a
maito174 <shigeru.komatsu@gmail.com>
2025-09-15 14:36:42 +0900

    changees for Azure static web deployment

--- src/components/config/commonConfig.ts
+++ src/components/config/commonConfig.ts
@@ -1,4 +1,4 @@
 export const CommonConfig: { API_BASE_URL: string } = {
-  API_BASE_URL: "https://dpwsa.apo.epson.net/cadg.api/api"
+  API_BASE_URL: "https://kmtest01-gcb9ghgqcmewdkfw.japanwest-01.azurewebsites.net/sampleap.api/api"
   //API_BASE_URL: 'https://localhost:7183/api',
 }
--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/sampleAp.vue/',
-  //base: '/',
+  //base: '/sampleAp.vue/',
+  base: '/',
 })
